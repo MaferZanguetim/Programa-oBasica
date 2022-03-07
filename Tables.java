e8fbbaba981c35fb924b56f4ccb984297e28fd79
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-03-07 19:29:40 +0000

    Mafer

--- Tables.java
+++ Tables.java
@@ -7,18 +7,9 @@ public class Tables{
         System.out.println("Digite um número: ");
         valor=escrita.nextInt();
 
-        switch (valor){
-            case 1:
-                System.out.println("Valor = 1");
-                break;
-            case 2:
-                System.out.println("Valor = 2");
-                break;
-            case 3: 
-                System.out.println("Valor = 3");
-                break;
-            default:
-            System.out.println("Nenhum dos 3 valores");
-        }
+       while (valor < 18) {
+           System.out.println("Mafer Zanchettim");
+           valor++;
+       }
     }
 }
